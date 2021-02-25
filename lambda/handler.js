90d64a00c8d477c91292e91b0aa767eb854c05a9
Dan Snow <snow.dfs@gmail.com>
2021-02-24 23:06:13 -0600

    Add lambda@edge function

---
+++ lambda/handler.js
@@ -0,0 +1,16 @@
+// lambda@edge function to inject cache control header into S3 content
+exports.handler = async (event, context) => {
+    const response = event.Records[0].cf.response;
+    const headers = response.headers;
+    const defaultTimeToLive = 60 * 60 * 24 * 180; // 180 days
+
+    if (!headers['cache-control']) {
+      headers['cache-control'] = [{
+        key: 'Cache-Control',
+        value: `public, max-age=${defaultTimeToLive}, immutable`,
+      }];
+      console.log('Response header modified!');
+    }
+
+    return response;
+};
