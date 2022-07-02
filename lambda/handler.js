551ca64661497a670fc0c9ea16e1e5311671bded
Dan Snow <daniel.snow@cookcountyil.gov>
2022-07-02 15:12:17 -0500

    Reduce cache-control max age

--- lambda/handler.js
+++ lambda/handler.js
@@ -2,7 +2,7 @@
 exports.handler = async (event, context) => {
     const response = event.Records[0].cf.response;
     const headers = response.headers;
-    const defaultTimeToLive = 60 * 60 * 24 * 180; // 180 days
+    const defaultTimeToLive = 60 * 60 * 24 * 30; // 30 days
 
     if (!headers['cache-control']) {
       headers['cache-control'] = [{
