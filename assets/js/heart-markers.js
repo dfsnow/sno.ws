dac6f91ecc652af3899bc85ced704b124f239c23
Dan Snow <snow.dfs@gmail.com>
2022-08-21 00:29:21 -0500

    Disable ::marker content for Safari

--- assets/js/heart-markers.js
+++ assets/js/heart-markers.js
@@ -1,12 +1,14 @@
 // Mini-script to move hearts in text to the ::marker pseudo-element
 document.querySelector('article').classList.add('js-enabled')
 document.querySelector('article span.heart').classList.add('js-enabled')
+var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+var useMarker = CSS.supports('selector(::marker)')
 var lis = document.querySelectorAll('article.js-enabled ul li')
     oldHTML = 'innerHTML',
     newHTML = '';
 
 [].forEach.call(lis, function (a) {
-    if (a[oldHTML].includes('♥') & CSS.supports('selector(::marker)')) {
+    if (a[oldHTML].includes('♥') & useMarker & !isSafari) {
         a.className = 'with-heart';
         newHTML = a[oldHTML].replace('♥', '');
         a[oldHTML] = newHTML;
