bd8837fbb2a1cc383f13c928171ac3aef6d8d3f5
Dan Snow <dan@sno.ws>
2023-10-20 02:27:17 -0500

    Revert "Add Safari tweaks for markers"
    This reverts commit 80f6e2523562cb927554a5320d0b58d96f7892f6.

--- assets/js/heart-markers.js
+++ assets/js/heart-markers.js
@@ -2,19 +2,14 @@
 document.querySelector("article").classList.add("js-enabled");
 
 var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+var useMarker = CSS.supports("selector(::marker)");
 var lis = document.querySelectorAll("article.js-enabled ul li");
 
-lis.forEach(function(e) {
-    if (e.contains(e.querySelector("svg"))) {
-        e.classList.add("with-heart");
-        e.querySelectorAll('svg').forEach(s => s.remove())
-        if (isSafari) {
-            e.classList.add("safari-marker");
-        }
-    } else {
-        e.classList.add("no-heart");
-        if (isSafari) {
-            e.classList.add("safari-marker");
-        }
-    };
-});
+if (useMarker & !isSafari) {
+    lis.forEach(function(e) {
+        if (e.contains(e.querySelector("svg"))) {
+            e.classList.add("with-heart");
+            e.querySelectorAll('svg').forEach(s => s.remove())
+        };
+    });
+}
