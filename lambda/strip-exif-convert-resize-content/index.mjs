cec41479afb1c7c6cf48cdff66b04491ba84ac76
Dan Snow <dan@sno.ws>
2024-07-27 19:39:17 -0500

    Update to AWS SDK v3 and Node 20

--- lambda/strip-exif-convert-resize-content/index.mjs
+++ lambda/strip-exif-convert-resize-content/index.mjs
@@ -1,13 +1,11 @@
-const AWS = require("aws-sdk");
-const sharp = require("sharp");
-const path = require("path");
-const s3 = new AWS.S3();
+import { S3Client, GetObjectCommand, PutObjectCommand, CopyObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import sharp from "sharp";
+import path from "path";
 
-exports.handler = async (event) => {
-    // Read options from the event parameter
+const s3 = new S3Client({});
+
+export const handler = async (event) => {
     const srcBucket = event.Records[0].s3.bucket.name;
-
-    // Object key may have spaces or unicode non-ASCII characters
     const srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
     const srcKeyExt = path.extname(srcKey).toLowerCase();
 
@@ -19,22 +17,17 @@ exports.handler = async (event) => {
     const backupBucket = process.env.BACKUP_BUCKET;
     const backupPath = process.env.BACKUP_PATH;
 
-    // Check that the image type is supported
     if (![".jpg", ".png", ".gif"].includes(srcKeyExt)) {
         console.log("Unsupported image type: " + srcKeyExt);
         return;
     }
 
     try {
-        // Download the original image from the S3 source bucket
-        const params = {
-            Bucket: srcBucket,
-            Key: srcKey
-        };
-        var origimage = await s3.getObject(params).promise();
+        const origimage = await s3.send(new GetObjectCommand({ Bucket: srcBucket, Key: srcKey }));
+        const origimageByteArray = await origimage.Body.transformToByteArray();
+        const origimageBody = Buffer.from(origimageByteArray);
         console.log("Got file: " + srcBucket + "/" + srcKey);
 
-        // Resize image and upload each version
         for (let res of [640, 1280]) {
             const formats = [
                 { ext: srcKeyExt, func: (img) => img.rotate().resize(res) },
@@ -43,43 +36,34 @@ exports.handler = async (event) => {
             ];
 
             for (let format of formats) {
-                const buffer = await format.func(sharp(origimage.Body)).toBuffer();
-                const destParams = {
+                const buffer = await format.func(sharp(origimageBody)).toBuffer();
+                await s3.send(new PutObjectCommand({
                     Bucket: dstBucket,
                     Key: `${dstPathResized}/${dstKey}-${res}${format.ext}`,
                     Body: buffer,
                     ContentType: "image"
-                };
-                await s3.putObject(destParams).promise();
+                }));
                 console.log("Generated size/ext: " + res + format.ext);
-            }
-        }
+            };
+        };
 
-        // Strip original image of EXIF data and rotate
-        const buffer = await sharp(origimage.Body).rotate().toBuffer();
-        const destParams = {
+        const buffer = await sharp(origimageBody).rotate().toBuffer();
+        await s3.send(new PutObjectCommand({
             Bucket: dstBucket,
             Key: `${dstPathOriginal}/${dstKey}${srcKeyExt}`,
             Body: buffer,
             ContentType: "image"
-        };
-        await s3.putObject(destParams).promise();
+        }));
         console.log("Stripped " + srcBucket + "/" + srcKey);
 
-        // Copy the original image to the backup bucket, then delete
-        const copyParams = {
+        await s3.send(new CopyObjectCommand({
             Bucket: backupBucket,
             CopySource: `${srcBucket}/${srcKey}`,
             Key: `${backupPath}/${dstKey}${srcKeyExt}`
-        };
-        await s3.copyObject(copyParams).promise();
+        }));
         console.log("Copied original image to " + backupBucket);
 
-        const deleteParams = {
-            Bucket: srcBucket,
-            Key: srcKey
-        };
-        await s3.deleteObject(deleteParams).promise();
+        await s3.send(new DeleteObjectCommand({ Bucket: srcBucket, Key: srcKey }));
         console.log("Deleted image " + srcBucket + "/" + srcKey);
 
     } catch (error) {
